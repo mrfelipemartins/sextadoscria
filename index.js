89f3f331c70dd295ffafb7a39fe7f80e24020096
Felipe Martins <felipe.dev@virtuamax.com.br>
2021-08-27 13:50:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const exect = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
+
+const R = require('ramda');
+
+const mainPath = path.dirname(fs.realpathSync(__filename));
+const soundPath = path.join(mainPath, './sexta');
+
+const sextadoscria = function (){
+    const linuxcmd = R.join('', ['paplay ', soundPath, '.ogg']);
+    const windowscmd = R.join('', [path.join(mainPath, './forWindows.vbs'), ' ', soundPath, '.mp3']);
+    const maccmd = R.join('', ['afplay ', soundPath, '.mp3']);
+
+    const platform = process.platform;
+
+    R.cond([
+        [R.equals('linux'), exec(linuxcmd)],
+        [R.equals('win32'), exec(windowscmd)],
+        [R.equals('darwin'), exec(maccmd)],
+    ], platform)
+
+    function exec(cmd){
+        return exect(cmd, function (error) {
+            R.ifElse(
+               R.empty,
+               () => console.log('AIAIAIAIAIAIAI'),
+               (error) => console.error(error),
+               error)
+        });
+    }
+}
+
+module.exports = sextadoscria;
+
+if (!module.parent) {
+    sextadoscria();
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+# DaleDale Command Line Sound
+
+<div style="text-align: center">
+    <img src="./sexta.jpg" height="300"/>
+</div>
+
+## Compatibility
+
+- Linux
+- Windows
+- Mac
+
+## Installing
+
+Install globally:
+
+    npm install -g sextadoscria
+
+## Running
+
+Run command:
+
+    sextadoscria
+
+Or import in NodeJS script:
+
+    import daledale from 'sextadoscria';
+
+    sextadoscria();
+
+## Running with [npx](https://www.npmjs.com/package/npx)
+
+Run command:
+
+    npx sextadoscria
+
+## See also
+
+- [anabastos](https://github.com/anabastos/daledale)
+- [Oloquinho](https://github.com/oloquinho/oloquinho)
+- [xFiles](https://github.com/BrOrlandi/xfiles/)
+- [Faustão Errou](https://github.com/BrOrlandi/faustao-errou/)
+- [Omaewa](https://github.com/BrOrlandi/omaewa/)
